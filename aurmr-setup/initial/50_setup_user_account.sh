f56cbda2f542a2fd412c4d6322799ba8ea3b3fad
Markus Grotz <grotz@uw.edu>
2022-07-28 11:18:14 -0700

    reload bashrc so conda is available

--- aurmr-setup/initial/50_setup_user_account.sh
+++ aurmr-setup/initial/50_setup_user_account.sh
@@ -21,6 +21,9 @@ curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh 
 ./Miniconda3-latest-Linux-x86_64.sh
 
+# reload the bash
+source $HOME/.bashrc
+
 conda config --set auto_activate_base false
 conda install mamba -c conda-forge
 
