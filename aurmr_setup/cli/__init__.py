10fa10590e049fcc04f614eb1aea76463502ce10
Markus Grotz <grotz@uw.edu>
2022-07-31 14:09:20 -0700

    add log handler

--- aurmr_setup/cli/__init__.py
+++ aurmr_setup/cli/__init__.py
@@ -16,6 +16,7 @@ import questionary
 import system_scripts
 import user_scripts
 
+from rich.logging import RichHandler
 
 logger = logging.getLogger(__name__)
 
@@ -61,8 +62,28 @@ class QuestionaryChoice(click.Option):
 
 
 @click.group()
-def cli():
-    pass
+@click.option("--verbose", "-v", default=False, count=True,
+              help="Increase verbosity of the logging output. Can base used multiple times")
+@click.option("--quiet", "-q", default=False, is_flag=True,
+              help="Suppress all logging output except critical messages")
+def cli(**kwargs):
+    """
+    aurmr command line interface
+    """
+    log_config = {'handlers': [RichHandler()]}
+    if kwargs['verbose'] and kwargs['quiet']:
+        logger.error('verbose and quiet must be mutually exclusive')
+        sys.exit(-1)
+    elif kwargs['verbose'] == 1:
+        logging.basicConfig(level=logging.INFO, **log_config)
+    elif kwargs['verbose'] == 2:
+        logging.basicConfig(level=logging.DEBUG, **log_config)
+    elif kwargs['verbose'] >= 3:
+        logging.basicConfig(level=logging.NOTSET, **log_config)
+    elif kwargs['quiet']:
+        logging.basicConfig(level=logging.CRITICAL, **log_config)
+    else:
+        logging.basicConfig(level=logging.INFO, **log_config)
 
 
 #@cli.group()
