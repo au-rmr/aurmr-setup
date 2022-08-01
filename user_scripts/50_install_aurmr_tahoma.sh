dffcf38167e180f1183df34a286c25daba124e25
Markus Grotz <grotz@uw.edu>
2022-08-01 16:42:55 -0700

    check if WORKSPACE_NAME is set for installation scripts

--- user_scripts/50_install_aurmr_tahoma.sh
+++ user_scripts/50_install_aurmr_tahoma.sh
@@ -2,16 +2,17 @@
 
 if [ -n "$1" ]; then
 	echo "workspace name = $1"
+    WORKSPACE_NAME=$1
+elif [ -n "WORKSPACE_NAME" ]; then
+	echo "workspace name = $WORKSPACE_NAME"
 else
 	echo "Usage $0 <workspace_name>."
 	exit 1
 fi
 
-WORKSPACE_NAME=$1
-
 source "/home/aurmr/miniconda3/etc/profile.d/conda.sh"
-
 conda activate $WORKSPACE_NAME
+
 mkdir -p $HOME/workspaces/$WORKSPACE_NAME/src
 cd $HOME/workspaces/$WORKSPACE_NAME/src
 
