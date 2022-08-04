25a7ac33cf5e1a207c570020525c7af0fc0adb6c
Markus Grotz <markus.grotz@kit.edu>
2022-08-04 11:57:43 -0700

    add nlopt

--- system_scripts/60_install_dev_dependencies.sh
+++ system_scripts/60_install_dev_dependencies.sh
@@ -4,3 +4,6 @@ sudo apt install build-essential git libssl-dev libelf-dev cmake cmake-curses-gu
 sudo apt install python3-sip python3-sip-dev python3-pykdl
 
 sudo apt install terminator
+
+
+sudo apt install libnlopt0  libnlopt-dev libnlopt-cxx-dev libnlopt-cxx0
