09876f56c05d9732900b17748f0292e07a2f3980
Markus Grotz <grotz@uw.edu>
2023-01-05 13:43:52 -0800

    add cross mount to system setup

--- system_scripts/10_setup_system.sh
+++ system_scripts/10_setup_system.sh
@@ -7,6 +7,7 @@ sudo apt purge whoopsie
 # add essential tools
 sudo apt install vim tmux fzf zsh htop curl git
 sudo apt install net-tools
+sudo apt install lm-sensors
 
 # ssh  server
 sudo apt install openssh-server
@@ -28,7 +29,35 @@ ssh-keygen
 
 
 
-# misc
+# SSHFS
 
+mkdir /home/aurmr/workspaces
 sudo apt install fuse3 sshfs
-sudo apt install lm-sensors
+
+sudo mkdir /workspaces/
+sudo chown aurmr:aurmr /workspaces
+
+static_hostname=$(hostnamectl status --static)
+
+if [[ $static_hostname == "aurmr-perception" ]]; then
+	OTHER_HOST=control
+	THIS_HOST=perception
+	ln -s /home/aurmr/workspaces /workspaces/perception
+	mkdir /workspaces/control
+fi
+if [[ $static_hostname == "aurmr-control" ]]; then
+	OTHER_HOST=perception
+	THIS_HOST=control
+	ln -s /home/aurmr/workspaces /workspaces/control
+	mkdir workspaces/perception
+fi
+
+cat | sudo tee -a /etc/hosts <<EOF
+192.168.10.101 control aurmr-control
+192.168.10.102 perception aurmr-perception
+EOF
+
+cat | sudo tee -a /etc/fstab <<EOF
+aurmr@${OTHER_HOST}:/home/aurmr/workspaces/  /workspaces/$OTHER_HOST/  fuse.sshfs x-systemd.automount,_netdev,user,exec,transform_symlinks,identityfile=/home/aurmr/.ssh/id_rsa,allow_other,default_permissions,uid=aurmr,gid=aurmr 0 0
+EOF
+
