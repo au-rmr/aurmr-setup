a784565cf19ee5346b2e7ffd425ae833a963c26f
Markus Grotz <grotz@uw.edu>
2023-01-10 18:54:30 -0800

    draft for recipes

--- aurmr_setup/cli/recipes.py
+++ aurmr_setup/cli/recipes.py
@@ -9,20 +9,59 @@ import subprocess
 from importlib.resources import contents as files
 from importlib.resources import path
 
-
 import system_scripts
 import user_scripts
 
-import rich_click as click
-from rich.progress import Progress
-
-from click_prompt import choice_option
-
-import questionary
-
-from aurmr_setup.cli.main import cli
-from aurmr_setup.cli.workspace import get_all_workspaces
-
-
 logger = logging.getLogger(__name__)
 
+
+class Recipe:
+
+    def __init__(self, category, script_name: str):
+        self.category = category
+        self.script_name = script_name
+
+    @classmethod
+    @lru_cache(2)
+    def list_all(cls, category) -> List['Recipe']:
+        scripts = [os.path.splitext(s)[0]
+                   for s in files(category)
+                   if s.endswith('.sh')]
+        scripts = [Recipe(category, s) for s in scripts]
+        return sorted(scripts)
+
+    def execute(self, workspace=None):
+        script = f'{self.script_name}.sh'
+        my_env = os.environ.copy()
+        if workspace:
+            my_env['WORKSPACE_NAME'] = workspace.workspace_name
+        with path(self.category, script) as script_full_path:
+            logger.info('Running %s', script_full_path)
+            return subprocess.run(str(script_full_path), check=True, env=my_env)
+
+    def __str__(self):
+        return f'{self.script_name}'
+
+
+class UserRecipe(Recipe):
+
+    @classmethod
+    def all_user_scripts(cls):
+        return Recipe.list_all(user_scripts)
+
+    @classmethod
+    def all_user_scripts_str(cls):
+        return [str(r)
+                for r in UserRecipe.all_user_scripts()]
+
+class SystemRecipe(Recipe):
+
+    @classmethod
+    def all_system_scripts(cls):
+        return Recipe.list_all(system_scripts)
+
+
+    @classmethod
+    def all_system_scripts_str(cls):
+        return [str(r) 
+                for r in SystemRecipe.all_system_scripts()]
