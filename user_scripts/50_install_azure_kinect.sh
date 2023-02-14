f5dac45651ec288961570ffda90bf7daa50a586d
Markus Grotz <grotz@uw.edu>
2023-02-14 01:25:54 -0800

    fix installation for Azure Kinect

--- user_scripts/50_install_azure_kinect.sh
+++ user_scripts/50_install_azure_kinect.sh
@@ -5,8 +5,14 @@ source $basedir/activate_workspace.bashrc
 
 git clone git@github.com:microsoft/Azure_Kinect_ROS_Driver.git
 
+mkdir  deb
+apt download libk4a1.3 deb                                                                                                                                                                                                              
+apt download libk4a1.3-dev deb
+
+DEB_DIR=$(readlink -f deb)
 cd ..
 
-# catkin config --cmake-args -Dk4a_DIR:PATH=/usr/lib/x86_64-linux-gnu/cmake/k4a
 
-catkin build azure_kinect_ros_driver
+#catkin build azure_kinect_ros_driver
+
+catkin build azure_kinect_ros_driver -v --cmake-args  -Dk4a_DIR:PATH=${DEB_DIR}/usr/lib/x86_64-linux-gnu/cmake/k4a -Dk4arecord_DIR:PATH=${DEB_DIR}/usr/lib/x86_64-linux-gnu/cmake/k4arecord 
