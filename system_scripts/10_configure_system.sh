8481fc8a90340324e5d316e9909596fab9bcaa69
Markus Grotz <grotz@uw.edu>
2023-02-19 14:12:36 -0800

    enable no-install-recommends for apt

--- system_scripts/10_configure_system.sh
+++ system_scripts/10_configure_system.sh
@@ -4,6 +4,11 @@ echo "Disabling unattended upgrades"
 
 sudo dpkg-reconfigure unattended-upgrades
 
+cat | sudo tee -a /etc/apt/apt.conf.d/01norecommend << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
+
 
 # hostname
 echo """Set the hostname with
