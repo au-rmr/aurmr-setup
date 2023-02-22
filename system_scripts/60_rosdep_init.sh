74c2f6b84a40e7ab344a71299706c355656fd27b
Markus Grotz <grotz@uw.edu>
2023-02-21 22:37:47 -0800

    add 60_rosdep_init.sh script
    - fake rosdep init

---
+++ system_scripts/60_rosdep_init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -exu
+
+sudo mkdir -p /etc/ros/rosdep/sources.list.d                                                                                                                                     git:(franka-1|✚3…2 
+sudo touch /etc/ros/rosdep/sources.list.d/20-default.list      
