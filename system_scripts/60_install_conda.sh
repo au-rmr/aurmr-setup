ec6da6f45a13e3b0979fd310fc142ba0544876e4
Markus Grotz <grotz@uw.edu>
2023-02-24 00:03:50 -0800

    revise conda installation

--- system_scripts/60_install_conda.sh
+++ system_scripts/60_install_conda.sh
@@ -14,9 +14,12 @@ curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh 
 ./Miniconda3-latest-Linux-x86_64.sh
 
-# reload the bash
-source $HOME/.bashrc
+
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+
+conda init bash zsh
+
+conda install mamba -c conda-forge
 
 conda config --set auto_activate_base false
-conda install mamba -c conda-forge
 
