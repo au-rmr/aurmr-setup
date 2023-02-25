f916f9db2697e0a1e3f6edd914fe873004773257
UW Amazon Manipulation Project <amazon-manipulation@cs.washington.edu>
2023-02-25 14:54:56 -0800

    configure locale

--- system_scripts/10_setup_system.sh
+++ system_scripts/10_setup_system.sh
@@ -31,3 +31,14 @@ ssh-keygen
 
 
 mkdir $HOME/workspaces
+
+
+
+sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
