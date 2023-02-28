6775cf63456241bc32a112c0a6c5777cc41d0e3a
Markus Grotz <grotz@uw.edu>
2023-02-27 22:37:39 -0800

    add system config

--- aurmr_setup/core/config.py
+++ aurmr_setup/core/config.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
 
+@dataclass
+class SystemConfig:
+
+    WORKSPACE_DIR = '~/workspaces/'
+    ACTIVE_WORKSPACE = '~/.active_workspace'
+    ARCHIVE_DIR = 'archive'
+    ENVIRONMENT_FILE = 'environment.yml'
+
+
 
 @dataclass
 class WorkspaceConfig:
 
     rosdistro: str = 'noetic'
 
+system_config = SystemConfig()
