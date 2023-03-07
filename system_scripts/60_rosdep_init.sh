7d0bcc7b38866b7658d1dc1cdad1aa708f2cff24
Markus Grotz <grotz@uw.edu>
2023-03-06 16:10:04 -0800

    Remove shell symbols for rosdep commands

--- system_scripts/60_rosdep_init.sh
+++ system_scripts/60_rosdep_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -exu
 
-sudo mkdir -p /etc/ros/rosdep/sources.list.d                                                                                                                                     git:(franka-1|✚3…2 
-sudo touch /etc/ros/rosdep/sources.list.d/20-default.list      
+sudo mkdir -p /etc/ros/rosdep/sources.list.d
+sudo touch /etc/ros/rosdep/sources.list.d/20-default.list
