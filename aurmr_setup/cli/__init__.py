60b2891b65a5672bced795c564c35bac4fc9782f
Markus Grotz <grotz@uw.edu>
2024-02-15 15:30:44 -0800

    add start/stop/status to cli

--- aurmr_setup/cli/__init__.py
+++ aurmr_setup/cli/__init__.py
@@ -2,3 +2,5 @@ from aurmr_setup.cli.main_cli import cli
 from aurmr_setup.cli import recipes_cli
 from aurmr_setup.cli import workspace_cli
 from aurmr_setup.cli import environment_cli
+from aurmr_setup.cli import system_cli
+
