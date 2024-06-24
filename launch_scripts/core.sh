ed91a65a71bf0675dc34b1d86428831259954732
Markus Grotz <grotz@uw.edu>
2024-06-24 15:03:19 -0700

    update core launch script

--- launch_scripts/core.sh
+++ launch_scripts/core.sh
@@ -4,7 +4,7 @@
 
 echo "starting roscore ..."
 tmux new-session -d -s aurmr-inter-core
-tmux send-keys -t aurmr-inter-core:0 "activate aurmr_demo_perception"  C-m
+tmux send-keys -t aurmr-inter-core:0 "activate aurmr_demo"  C-m
 tmux send-keys -t aurmr-inter-core:0 "roscore" C-m
 
 sleep 1
@@ -12,8 +12,17 @@ echo -n "done"
 
 echo "starting azure kinect ..."
 tmux new-window -t aurmr-inter-core
-tmux send-keys -t aurmr-inter-core:1 "activate aurmr_demo_perception"  C-m
+tmux send-keys -t aurmr-inter-core:1 "activate aurmr_demo"  C-m
 tmux send-keys -t aurmr-inter-core:1 "roslaunch azure_kinect_ros_driver driver.launch point_cloud:=True" C-m
 echo -n "done"
+
+echo "starting audio capture  ..."
+tmux new-window -t aurmr-inter-core
+tmux send-keys -t aurmr-inter-core:2 "activate aurmr_audio"  C-m
+tmux send-keys -t aurmr-inter-core:2 "roslaunch audio_capture capture.launch
+" C-m
+echo -n "done"
+
+
 
 tmux att
